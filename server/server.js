4db4ee11df286fdbc45c757732748593e36782f5
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-25 15:42:38 +0530

    MongoDB connected

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
-
+const connectDB = require("./config/connect");
 const typeDefs = require("./graphql/schema");
+
 const { Query } = require("./graphql/resolvers/query");
+
+// Load env vars
+dotenv.config({ path: path.resolve(__dirname, "./config/config.env") });
+
+// Connect to database
+connectDB();
 
 const app = express();
 const server = new ApolloServer({
@@ -15,6 +24,7 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
