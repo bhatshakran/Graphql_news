8250be754dcd96ebc94e17b7020409d7ef45dcfc
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-25 18:35:54 +0530

    Get user by Id

--- server/graphql/resolvers/query.js
+++ server/graphql/resolvers/query.js
@@ -1,7 +1,20 @@
+const { AuthenticationError } = require("apollo-server-express");
+const User = require("../../models/User");
+
 module.exports = {
   Query: {
-    hello: () => {
-      return "Hello Back!";
+    getUser: async (parent, args, context, info) => {
+      try {
+        const user = await User.findById(args.id);
+
+        if (!user) {
+          throw new AuthenticationError("User could not be found!");
+        }
+
+        return user;
+      } catch (err) {
+        throw err;
+      }
     },
   },
 };
