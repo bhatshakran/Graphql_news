9248e1dc7cfa8398eda9c40ce81ff95d3bcaea50
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-27 14:37:23 +0530

    Create post model schema

---
+++ server/models/Post.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+      maxlength: 100,
+    },
+    excerpt: {
+      required: true,
+      type: String,
+      maxlength: 1000,
+    },
+    content: {
+      required: true,
+      type: String,
+      maxlength: 100000,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: "DRAFT",
+    },
+    status: {
+      type: String,
+      enum: ["DRAFT", "PUBLIC"],
+      required: true,
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
