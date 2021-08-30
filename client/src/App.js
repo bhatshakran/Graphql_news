7e153db9a686a087c452eeaeddc4955e0461da7f
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-30 16:25:52 +0530

    Created home page and a header page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./components/Home.js";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Container className="mt-4">
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
