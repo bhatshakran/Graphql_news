48e062f3728988be84a228227363c759988adeb2
Bhat Shaqran <bhatshakran@gmail.com>
2021-09-01 11:00:45 +0530

    Created status file

--- client/src/components/userArea/profile/index.js
+++ client/src/components/userArea/profile/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserAreaHOC from "../../HOC/UserAreaHOC";
 import EmailPass from "./emailPass";
+import Stats from "./stats";
 
 const Profile = (props) => {
   return (
     <UserAreaHOC>
       <EmailPass {...props} />
       <hr />
+      <Stats {...props} />
     </UserAreaHOC>
   );
 };
