6c5d83a79d08a2503f4bafb5826bfb7271ade071
Bhat Shaqran <bhatshakran@gmail.com>
2021-09-01 15:22:51 +0530

    Removed vscode from remote repo

--- client/src/redux/features/posts/posts.js
+++ client/src/redux/features/posts/posts.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import config from "../../../axios/config";
+import toastHandler from "../../../utils/toasts";
 
 export const createPost = createAsyncThunk("/createpost", async (args) => {
   try {
@@ -16,10 +17,12 @@ export const createPost = createAsyncThunk("/createpost", async (args) => {
     };
 
     const { data } = await config({ data: JSON.stringify(body) });
+    toastHandler("Post created", "SUCCESS");
 
     return data.data.createPost;
   } catch (err) {
     console.log(err);
+    toastHandler(["Failed to create the post", "Try again!"], "SUCCESS");
   }
 });
 
@@ -28,10 +31,17 @@ export const postsSlice = createSlice({
   initialState: {
     loading: true,
     message: "",
-    posts: {},
+    posts: [],
+    post: {},
   },
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [createPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.message = "Post added to the DB!";
+      state.post = action.payload;
+    },
+  },
 });
 
 export default postsSlice.reducer;
