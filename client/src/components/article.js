f9d6480c241b44d75b7401d3044f6c1f76cc6ecc
Bhat Shaqran <bhatshakran@gmail.com>
2021-09-04 17:47:32 +0530

    Render post details on the page

--- client/src/components/article.js
+++ client/src/components/article.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CardItem from "react-bootstrap";
 import { CardGroup } from "react-bootstrap";
 import { getPost } from "../redux/features/posts/posts";
 
 const Article = (props) => {
-  const posts = useSelector((state) => state.posts);
+  const post = useSelector((state) => state.posts.post);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,7 +14,21 @@ const Article = (props) => {
     }
   }, [dispatch]);
 
-  return <div>article here</div>;
+  return (
+    <Fragment>
+      {Object.keys(post).length > 0 ? (
+        <Fragment>
+          <h1>{post.title}</h1>
+          <small>
+            Created by {post.author.name}
+            {post.author.lastname}
+          </small>
+          <hr />
+          <div>{post.content}</div>
+        </Fragment>
+      ) : null}
+    </Fragment>
+  );
 };
 
 export default Article;
